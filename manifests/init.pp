567af7ea50757efefa7f1109ff0c26838bb73f8c
Lindsey Smith <lindsey.smith@gmail.com>
2015-09-10 21:31:40 +0000

    puppet-lint fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,7 +32,8 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
+
 class envnotification {
-   notify{"This puppet run happened in environment: \"${environment}\"":}
-   notify{"This agent reported agent_specified_environment: \"$agent_specified_environment\"":}
+  notify{"This puppet run happened in environment: \"${environment}\"":}
+  notify{"This agent reported agent_specified_environment: \"${::agent_specified_environment}\"":}
 }
